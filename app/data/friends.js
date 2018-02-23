2cf4996c8e589e7900481d112429d1728daab971
Kelly <kelly.hagan3@gmail.com>
2018-02-23 11:06:22 -0600

    adds correct link to css and images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,82 +1,66 @@
 var friendsArr = [
     {
-    "name": "Alex",
-    "photo": "images/alex.jpg",
+    "name": "Ski",
+    "photo": "/images/ski.jpg",
     "scores": [
-        5,
         1,
         4,
         4,
-        5,
-        1,
         2,
+        4,
+        5,
         5,
         4,
-        1
+        2,
+        4
         ]
     },
     {
-    "name": "Lisa",
-    "photo": "images/lisa.png",
+    "name": "Europe",
+    "photo": "/images/prague.jpg",
     "scores": [
         4,
-        2,
-        5,
-        1,
         3,
         2,
+        3,
         2,
         1,
-        3,
+        1,
+        1,
+        2,
         2
         ]
     },
     {
-    "name": "Colin",
-    "photo": "https://scontent.ford1-1.fna.fbcdn.net/v/t31.0-8/21125755_10214571860189207_2596615415025455811_o.jpg?oh=b8d1f75d867a735b4ccf459fdb827601&oe=5B19B6AA",
+    "name": "Winery",
+    "photo": "/images/winery.jpg",
     "scores": [
-    5,
-    2,
-    2,
-    2,
     3,
-    4,
+    3,
+    1,
+    5,
+    1,
     1,
     2,
-    5,
-    5
+    2,
+    4,
+    2
     ]
     },
-    // {
-    // "name": "Sean",
-    // "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    // "scores": [
-    // 3,
-    // 3,
-    // 4,
-    // 2,
-    // 2,
-    // 1,
-    // 3,
-    // 2,
-    // 2,
-    // 3
-    // ]
-    // },
     {
-    "name": "Kevin",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "name": "Beach",
+    "photo": "/images/beach.jpg",
     "scores": [
-    4,
-    3,
-    4,
-    1,
-    5,
     2,
-    5,
+    1,
+    1,
     3,
     1,
-    4
+    2,
+    2,
+    2,
+    1,
+    2
     ]
     }
 ];
